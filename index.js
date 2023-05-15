f7b24d4a8182926807370300d8cdca48b41c7355
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2023-05-15 17:10:07 +0300

    Install CORS library and use it

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express, { json } from "express";
 import todosRouter from "./routes/todosRouter.js";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+  })
+);
 const port = 4444;
 app.use(express.static("public"));
 app.use(json());
